d58b7d3033bb8075bffbd6859db3b0e7e4657247
Mike Zazaian <mike@functionhaus.com>
2019-01-12 17:25:52 +0100

    flesh out license, readme, mixfile

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # VerifyRecaptcha
 
-**TODO: Add description**
+VerifyRecaptcha consists of a single Plug module
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `verify_recaptcha` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `verify_recaptcha` to your list of
+dependencies in `mix.exs`:
 
 ```elixir
 def deps do
   [
-    {:verify_recaptcha, "~> 0.1.0"}
+    {:verify_recaptcha, "~> 0.0"}
   ]
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/verify_recaptcha](https://hexdocs.pm/verify_recaptcha).
+## Availability
 
+This library is currently published at for use with the public hex.pm
+repository at https://hex.pm/packages/verify_recaptcha.
+
+Source code is available at the [FunctionHaus Github Organization](
+https://github.com/functionhaus) at
+https://github.com/functionhaus/verify_recaptcha.
+
+## License
+
+ConnArtist source code is released under Apache 2 License.
+Check LICENSE file for more information.
--- mix.exs
+++ mix.exs
@@ -4,25 +4,52 @@ defmodule VerifyRecaptcha.MixProject do
   def project do
     [
       app: :verify_recaptcha,
-      version: "0.1.0",
+      name: "VerifyRecaptcha",
+      version: "0.0.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env() == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/functionhaus/verify_recaptcha",
+      homepage_url: "https://functionhaus.com",
+
+      docs: [
+        main: "VerifyRecaptcha", # The main page in the docs
+        logo: "assets/functionhaus_logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+     {:plug, "~> 1.7"},
+     {:recaptcha, "~> 2.3"},
+     {:conn_artist, "~> 0.0"},
+     {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
+  end
+
+  defp description do
+    "A Plug-compatible module for handling live Recaptcha responses."
+  end
+
+  defp package do
+    [
+     files: ["lib", "mix.exs", "README.md"],
+     maintainers: ["FunctionHaus LLC, Mike Zazaian"],
+     licenses: ["Apache 2"],
+
+     links: %{"GitHub" => "https://github.com/functionhaus/verify_recaptcha",
+              "Docs" => "https://hexdocs.pm/verify_recaptcha"}
+     ]
   end
 end
